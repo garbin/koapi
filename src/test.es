bead6ef310eda85e73832a39357627f326ab5a60
Garbin Huang <garbinh@gmail.com>
2016-07-30 10:11:59 +0000

    + add test framework

---
+++ src/test.es
@@ -0,0 +1,103 @@
+import chai from 'chai'
+import _ from 'lodash'
+import qs from 'qs'
+import test from 'ava'
+import {expect} from 'chai'
+
+chai.use(require('chai-http'));
+
+export const request = chai.request;
+export {test, expect}
+export const suite = (cb) => {
+  return cb(ResourceTester, test, expect);
+};
+
+export const HttpTester = class  {
+  request = null;
+  promise = null;
+  config = {
+    title:'',
+    expect: function(){}
+  };
+  expects = function(){};
+  constructor(server) {
+    this.request = request(server);
+  }
+  req(cb){
+    this.promise = cb(this.request);
+    return this;
+  }
+  title(title){
+    this.config.title = title;
+  }
+  expect(cb){
+    this.config.expect = cb;
+  }
+  test(cb){
+    cb = cb || function(res){return res;};
+    return test(this.config.title,t => this.promise.then(_.wrap(this.config.expect, (func, res)=>{
+      func(res);
+      cb(res);
+      return res;
+      // return cb(res);
+    })))
+  }
+}
+
+export const ResourceTester = class  {
+  constructor(server, endpoint) {
+    this.endpoint = endpoint;
+    this.server   = server;
+  }
+  create(resource){
+    let tester = new HttpTester(this.server);
+    tester.title(`POST ${this.endpoint}`);
+    tester.req(req => req.post(this.endpoint)
+    .set('Accept', 'application/json')
+    .send(resource));
+    tester.expect(res => {
+      expect(res).to.have.status(201);
+      expect(res.body.id).to.be.a('number')
+    });
+
+    return tester;
+  }
+  read(id, query = ''){
+    query = _.isString(query) ? query : qs.stringify(query);
+    let path = this.endpoint + (id ? '/' + id : '') + (query ? '?' + query : '');
+    let tester = new HttpTester(this.server);
+    tester.title(`GET ${path}`);
+    tester.req(req => req.get(path).set('Accept', 'application/json'));
+    tester.expect(res => {
+      expect(res).to.have.status(200);
+      if (!id) expect(res.body).to.be.an('array');
+    });
+
+    return tester;
+  }
+  update(id, data, query){
+    let path = this.endpoint + (id ? '/' + id : '') + (query ? '?' + query : '');
+    let tester = new HttpTester(this.server);
+    tester.title(`PATCH ${path}`);
+    tester.req(req => req.patch(path).set('Accept', 'application/json').send(data));
+    tester.expect(res => {
+      expect(res).to.have.status(202);
+      _.forIn(data, (v, k)=>{
+        expect(res.body[k]).equals(v);
+      });
+    });
+
+    return tester;
+  }
+  destroy(id, query){
+    let path = this.endpoint + (id ? '/' + id : '') + (query ? '?' + query : '');
+    let tester = new HttpTester(this.server);
+    tester.title(`DELETE ${path}`);
+    tester.req(req => req.del(path).set('Accept', 'application/json'));
+    tester.expect(res => {
+      expect(res).to.have.status(204)
+    });
+
+    return tester;
+  }
+}
--- test/resource_router.test.js
+++ test/resource_router.test.js
@@ -1,7 +1,6 @@
-import request from 'supertest-as-promised'
 import Koapi, {Model, ResourceRouter} from '../src'
+import {suite, ResourceTester, expect} from '../src/test'
 import knex_config from './knex/knexfile'
-import {describe} from 'ava-spec'
 
 Model.init(knex_config.test);
 
@@ -23,109 +22,45 @@ const setup = (config) => {
   return {app, server};
 };
 
-describe('ResourceRouter', it => {
-  let {server, app} = setup(app => {
-    let posts = new ResourceRouter(Post.collection());
-    posts.create();
-    posts.read({
-      sortable: ['created_at'],
-      filterable: ['user_id'],
-      searchable: ['title', 'content']
-    });
-    posts.update();
-    posts.destroy();
-    let comments = new ResourceRouter(ctx => Comment.collection());
-    comments.crud();
-    app.bodyparser({
-        fieldsKey: false,
-        filesKey: false,
-    });
-    app.routers( [ posts, posts.use('/posts/:post_id/comments', comments.routes()) ] );
+let {server, app} = setup(app => {
+  let posts = new ResourceRouter(Post.collection());
+  posts.create();
+  posts.read({
+    sortable: ['created_at'],
+    filterable: ['user_id'],
+    searchable: ['title', 'content']
   });
+  posts.update();
+  posts.destroy();
+  let comments = new ResourceRouter(ctx => Comment.collection());
+  comments.crud();
+  app.bodyparser({
+    fieldsKey: false,
+    filesKey: false,
+  });
+  app.routers( [ posts, posts.use('/posts/:post_id/comments', comments.routes()) ] );
+});
 
-  it.serial('#list', t => request(server)
-                    .get('/posts')
-                    .set('Accept', 'application/json')
-                    .expect(200)
-                    .expect(res => res.should.be.json())
-                    .expect('Content-Range', 'items 0-0/1')
-                    .expect(res => res.body.length.should.be.equal(1)));
-  it.serial('get item', t => request(server)
-                        .get('/posts/1')
-                        .set('Accept', 'application/json')
-                        .expect(200)
-                        .expect(res => res.should.be.json())
-                        .expect(res => res.body.id.should.be.equal(1)));
-  it.serial('post item', t => request(server)
-                        .post('/posts')
-                        .set('Accept', 'application/json')
-                        .send({
-                          title: 'title',
-                          content: 'content'
-                        })
-                        .expect(201)
-                        .expect(res => res.should.be.json())
-                        .expect(res => res.body.id.should.be.equal(2)));
-  it.serial('patch item', t => {
-    let new_title = 'new title';
-    return request(server)
-      .patch('/posts/1')
-      .set('Accept', 'application/json')
-      .send({
-        title: new_title
-      })
-      .expect(202)
-      .expect(res => res.should.be.json())
-      .expect(res => res.body.title.should.be.equal(new_title))
+
+
+suite(function(){
+  let tester = new ResourceTester(server, '/posts');
+  tester.create({ title: 'title', content: 'content'}).test();
+  tester.read(1).test();
+  tester.update(1, {title: 'new title'}).test();
+  tester.destroy(2).test();
+  tester.read(null, {
+    filters:{
+      user_id:2
+    },
+    q:'doestnotexists'
+  }).test(res => {
+    expect(res.body).to.be.empty;
   });
-  it.serial('put item', t => {
-    let new_title = 'search';
-    return request(server)
-      .put('/posts/1')
-      .set('Accept', 'application/json')
-      .send({
-        title: new_title
-      })
-      .expect(202)
-      .expect(res => res.should.be.json())
-      .expect(res => res.body.title.should.be.equal(new_title))
-  });
-  it.serial('#list sort', t => request(server)
-                          .get('/posts?sort=created_at')
-                          .set('Accept', 'application/json')
-                          .expect(200)
-                          .expect(res => res.should.be.json())
-                          .expect(res => res.body[0].id.should.be.equal(1)));
-  it.serial('#list search', t => request(server)
-                            .get('/posts?q=doestnotexists')
-                            .set('Accept', 'application/json')
-                            .expect(200)
-                            .expect(res => res.should.be.json())
-                            .expect(res => res.body.should.be.empty()));
-  it.serial('#list filter', t => request(server)
-                            .get('/posts?filters[user_id]=2')
-                            .set('Accept', 'application/json')
-                            .expect(200)
-                            .expect(res => res.should.be.json())
-                            .expect(res => res.body.should.be.empty()));
-  it.serial('#list filter & search', t => request(server)
-                                    .get('/posts?filters[user_id]=2&q=doestnotexists')
-                                    .set('Accept', 'application/json')
-                                    .expect(200)
-                                    .expect(res => res.should.be.json())
-                                    .expect(res => res.body.should.be.empty()));
-  it.serial('nested #list', t => request(server)
-                            .get('/posts/1/comments')
-                            .set('Accept', 'application/json')
-                            .expect(200)
-                            .expect(res => res.should.be.json()));
-  it.serial('nested #item', t => request(server)
-                            .get('/posts/1/comments/1')
-                            .set('Accept', 'application/json')
-                            .expect(200)
-                            .expect(res => res.should.be.json()));
-  it.serial('delete item', t => request(server)
-                          .del('/posts/1')
-                          .set('Accept', 'application/json')
-                          .expect(204));
+});
+
+suite(function(){
+  let tester = new ResourceTester(server, '/posts/1/comments');
+  tester.read(1).test();
+  tester.read().test();
 });
