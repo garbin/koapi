8c385f5f436e0f2d5683a8b35f377e8a36ecad8b
Garbin Huang <garbinh@gmail.com>
2016-08-06 10:58:23 +0800

    * remove unusable package & rewrite koapi.test

--- test/koapi.test.js
+++ test/koapi.test.js
@@ -1,6 +1,5 @@
-import request from 'supertest-as-promised'
 import Koapi, {Model, Router, middlewares} from '../src';
-import {describe} from 'ava-spec'
+import suite from '../src/test'
 
 const setup = (config) => {
   let app = new Koapi();
@@ -9,54 +8,73 @@ const setup = (config) => {
   return {app, server};
 }
 
-describe('basic server', it => {
-  let {server} = setup(app => app);
-  it('should get 404 not found', t => request(server).get('/').set('Accept', 'application/json').expect(404))
-})
 
-describe('use router', it => {
+suite(({test, expect, request}) => {
+  let {server} = setup(app => app);
+  test('basic server should get 404 not found', t => {
+      return request(server).get('/')
+                            .set('Accept', 'application/json')
+                            .then(res => {})
+                            .catch(res => expect(res).to.have.status(404));
+    }
+  )
+});
+
+
+suite(({test, expect, request}) => {
   let {server, app} = setup(app => {
     app.routers([
       (new Router).get('/', ctx => ctx.body = 'Hello World').routes(),
       (new Router).get('/test', ctx => ctx.body = 'test')
     ]);
   });
-  it('should get 200 ok', t => request(server)
+  test('should get 200 ok', t => request(server)
                                 .get('/')
-                                .expect(res => res.text.should.be.equal('Hello World'))
-                                .expect(200))
-  it('should get 200 ok', t => request(server)
+                                .then(res => {
+                                  expect(res).to.have.status(200);
+                                  expect(res.text).equals('Hello World')
+                                }))
+  test('should get 200 ok', t => request(server)
                                 .get('/test')
-                                .expect(res => res.text.should.be.equal('test'))
-                                .expect(200))
-  it('should have _specs', t => request(server)
+                                .then(res => {
+                                  expect(res).to.have.status(200);
+                                  expect(res.text).equals('test')
+                                }))
+  test('should have _specs', t => request(server)
                                   .get('/_specs')
                                   .set('Accept', 'application/json')
-                                  .expect(res => res.should.be.json())
-                                  .expect(res => res.body.should.have.property('/'))
-                                  .expect(200))
+                                  .then(res => {
+                                    expect(res).to.be.json;
+                                    expect(res).to.have.status(200);
+                                  }))
 });
 
-describe('subdomain middleware', it => {
+suite(({test, request, expect}) => {
   let {server, app} = setup(app => {
     app.routers([
       middlewares.subdomain('api.*', (new Router).get('/', ctx => ctx.body = 'api').routes()),
       (new Router).get('/', ctx => ctx.body = 'index').routes(),
     ]);
   });
-  it('should get index', t => request(server)
+
+  test('subdomain should get index', t => request(server)
                                 .get('/')
-                                .expect(res => res.text.should.be.equal('index'))
-                                .expect(200))
-  it('should get api', t => request(server)
+                                .then(res => {
+                                  expect(res.text).equals('index');
+                                  expect(res).to.have.status(200);
+                                }))
+  test('subdomain should get api', t => request(server)
                               .get('/')
                               .set('Host', 'api.test.com')
-                              .expect(res => res.text.should.be.equal('api'))
-                              .expect(200));
-  it('should have _specs', t => request(server)
+                              .then(res => {
+                                expect(res).to.have.status(200);
+                                expect(res.text).equals('api');
+                              }));
+  test('subdomain should have _specs', t => request(server)
                                   .get('/_specs')
                                   .set('Accept', 'application/json')
-                                  .expect(res => res.should.be.json())
-                                  .expect(res => res.body.should.have.property('://api.*/'))
-                                  .expect(200))
+                                  .then(res => {
+                                    expect(res).to.be.json;
+                                    expect(res).to.have.status(200);
+                                  }));
 });
