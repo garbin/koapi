4cc1976899e52625002d34a07f5c856389fff0db
Garbin <garbinh@gmail.com>
2017-04-04 18:33:54 +0800

    rename commands -> cli

--- bin/koapi.js
+++ bin/koapi.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
-require('../lib').commands()
+const cwd = process.cwd()
+const pkg = require(`${cwd}/package`)
+const commands = require(`${cwd}/${pkg.koapi.commands.path}`)
+process.argv = !process.argv[2] ? process.argv.concat(pkg.koapi.commands.default || []) : process.argv
+require('../lib').cli(commands)
