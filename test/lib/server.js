9bca1bd63c256581fa08b810332a371d592c9696
Garbin Huang <garbinh@gmail.com>
2018-08-17 14:35:57 +0800

    update deps

--- test/lib/server.js
+++ test/lib/server.js
@@ -3,57 +3,67 @@ const { get } = require('lodash')
 const { connection, Comment, Post, Category, Test } = require('./models')
 const { schema } = require('./graphql')
 
-const setup = (config) => {
+const setup = config => {
   const app = new Koapi()
   app.use(middlewares.preset('restful'))
   config(app)
   const server = app.listen(null)
   server.on('close', () => connection.destroy())
-  return {app, server}
+  return { app, server }
 }
 
-const {server, app} = setup(app => {
+const { server, app } = setup(app => {
   const comments = router.resource(Comment, {
     collection: ctx => ctx.state.nested.post.comments()
   })
-  const posts = router.resource(Post, router => {
-    const before = async (ctx, next) => {
-      if (!ctx.request.query.before) {
-        return ctx.throw('before is required')
+  const posts = router
+    .resource(Post, router => {
+      const before = async (ctx, next) => {
+        if (!ctx.request.query.before) {
+          return ctx.throw('before is required')
+        }
+        await next()
       }
-      await next()
-    }
-    router.create(before, async (ctx, next) => {
-      ctx.state.attributes = ctx.request.body
-      ctx.state.attributes.title = 'Hehe'
-      await next()
-      ctx.body = ctx.body.toJSON()
-      ctx.body.haha = 'yes'
-    })
-    router.read(before, async (ctx, next) => {
-      if (get(ctx.request.query, 'filters.category_id')) {
-        ctx.state.query = Post.collection().query(q => {
-          return q.leftJoin('category2post', 'posts.id', 'category2post.post_id')
-        })
-      }
-      await next()
-    }, {
-      list: {
-        sortable: ['created_at'],
-        // filterable: [ 'user_id', 'category_id' ],
-        filterable: ({filter, query, filters}) => {
-          filter('user_id')
-          filter('category_id')
-          if (filters.tag) {
-            query.whereRaw('tags::jsonb @> ?', `"${filters.tag}"`)
+      router.create(before, async (ctx, next) => {
+        ctx.state.attributes = ctx.request.body
+        ctx.state.attributes.title = 'Hehe'
+        await next()
+        ctx.body = ctx.body.toJSON()
+        ctx.body.haha = 'yes'
+      })
+      router.read(
+        before,
+        async (ctx, next) => {
+          if (get(ctx.request.query, 'filters.category_id')) {
+            ctx.state.query = Post.collection().query(q => {
+              return q.leftJoin(
+                'category2post',
+                'posts.id',
+                'category2post.post_id'
+              )
+            })
           }
+          await next()
         },
-        searchable: ['title', 'content']
-      }
+        {
+          list: {
+            sortable: ['created_at'],
+            // filterable: [ 'user_id', 'category_id' ],
+            filterable: ({ filter, query, filters }) => {
+              filter('user_id')
+              filter('category_id')
+              if (filters.tag) {
+                query.whereRaw('tags::jsonb @> ?', `"${filters.tag}"`)
+              }
+            },
+            searchable: ['title', 'content']
+          }
+        }
+      )
+      router.update(before)
+      router.destroy(before)
     })
-    router.update(before)
-    router.destroy(before)
-  }).children(comments)
+    .children(comments)
   const aggregate = router.aggregate(router => {
     router.aggregate(Post, {
       filterable: ['test1'],
@@ -64,20 +74,18 @@ const {server, app} = setup(app => {
           name: 'created_date'
         }
       ],
-      metrics: [
-        { name: 'total', aggregate: 'count', column: 'id as total' }
-      ]
+      metrics: [{ name: 'total', aggregate: 'count', column: 'id as total' }]
     })
   })
   const categories = router.resource(Category)
-  app.use(middlewares.graphql('/graphql', ctx => ({
+  middlewares.graphql(app, ctx => ({
     context: { loader: new graphql.Loader() },
     schema,
     formatErros: e => {
       console.error(e)
       return e
     }
-  })))
+  }))
   app.use(middlewares.routers([posts, aggregate, categories]))
 })
 module.exports = { server, app, Category, Post, Comment, Test }
